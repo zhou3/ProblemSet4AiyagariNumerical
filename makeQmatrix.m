9d0dc50e48b833a152e4e3eca6c6ce3f57e0502e
Florian Kuhn <floriankuhn@users.noreply.github.com>
2016-03-03 15:32:30 -0500

    code for computing Q matrix in PFI

---
+++ makeQmatrix.m
@@ -0,0 +1,32 @@
+function Q = makeQmatrix(pol_indx, PI)
+% computes matrix Q for policy function iteration
+%
+% pol_indx: m-by-n matrix of indices of the endogenous variable chosen
+% according to the policy function
+%
+% PI: m-by-m stochastic transition matrix of the exogenous state variable
+%
+% Q: the m*n-by-m*n matrix to compute expected value of following the
+% policy
+
+m = length(PI);
+n = size(pol_indx, 2);
+Q = sparse(m*n, m*n);
+
+% sequential way to construct Q:
+for xi = 1:n
+    for zi = 1:m
+        row = (xi - 1) * m +  zi;
+        col = (pol_indx(zi, xi) - 1) * m;
+        Q(row, col+1:col+m) = PI(zi,:);
+    end
+end
+
+% alternative, vectorized way:
+rows = kron((1:m*n)', ones(m,1));
+cols = kron((pol_indx(:) - 1) * m, ones(m,1)) + kron(ones(m*n,1), (1:m)');
+vals = repmat([PI(1,:)'; PI(2,:)'], [n, 1]);
+Q = sparse(rows, cols, vals);
+
+
+
