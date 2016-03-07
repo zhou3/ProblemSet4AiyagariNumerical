adcf241dcdc6d8d05f746a4a4b0ca0d2dbf97ba4
Florian Kuhn <floriankuhn@users.noreply.github.com>
2016-03-07 13:28:36 -0500

    fixed makeQmatrix for case m>2

--- makeQmatrix.m
+++ makeQmatrix.m
@@ -24,9 +24,10 @@ end
 
 % alternative, vectorized way:
 rows = kron((1:m*n)', ones(m,1));
-cols = kron((pol_indx(:) - 1) * m, ones(m,1)) + kron(ones(m*n,1), (1:m)');
-vals = repmat([PI(1,:)'; PI(2,:)'], [n, 1]);
-Q = sparse(rows, cols, vals);
+cols = kron((pol_indx(:) - 1) * m, ones(m,1)) + kron(ones(m*n, 1), (1:m)');
+% vals = repmat([PI(1,:)'; PI(2,:)'], [n, 1]);
+vals = repmat( reshape( permute( PI, [2 1] ), [], 1) , [n, 1]);
+Q = sparse(rows, cols, vals, m*n, m*n);
 
 
 
